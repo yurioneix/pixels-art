31e46c45ea988d8d3a09942ba0ccde164f3359c8
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-04 15:53:56 -0300

    Cria section 'color-palette'

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ let createH1 = document.createElement('h1');
 createH1.id = 'title';
 createH1.innerText = 'Paleta de Cores';
 body.appendChild(createH1);
+
+let createSectionColorPalette = document.createElement('section');
+createSectionColorPalette.id = 'color-palette';
+body.appendChild(createSectionColorPalette);
+
+
