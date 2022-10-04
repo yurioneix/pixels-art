21509d5ee5791e0e69906a76240c1cf8fe372527
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-04 15:46:28 -0300

    Requisito 1: adiciona h1 de id title e texto 'Paleta de Cores'

---
+++ script.js
@@ -0,0 +1,9 @@
+//Captura dos elementos
+const body = document.getElementsByTagName('body')[0];
+console.log(body);
+
+//Criação dos elementos
+let createH1 = document.createElement('h1');
+createH1.id = 'title';
+createH1.innerText = 'Paleta de Cores';
+body.appendChild(createH1);
