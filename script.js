99333b448060fa47fb21a278326a08cae4ff3f05
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-04 17:51:17 -0300

    Melhora layout no style.css e  define cores para as divs de classe color e cria função de cor aleatória

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 //Captura dos elementos
 const body = document.getElementsByTagName('body')[0];
 
-//Criação dos elementos
+//Criação do h1
 let createH1 = document.createElement('h1');
 createH1.id = 'title';
 createH1.innerText = 'Paleta de Cores';
 body.appendChild(createH1);
 
+//Criação da section de id = 'color-palette'
 let createSectionColorPalette = document.createElement('section');
 createSectionColorPalette.id = 'color-palette';
 body.appendChild(createSectionColorPalette);
 
+//Laço for que cria 4 divs de classe e borda definidas, filhas da section color-palette 
 for (let index = 0; index < 4; index += 1) {
-let createDivColor = document.createElement('div');
-createDivColor.className = 'color';
-createDivColor.style.border = '1px solid black';
-const sectionColorPalette = document.querySelector('#color-palette');
-sectionColorPalette.appendChild(createDivColor);
+    let createDivColor = document.createElement('div');
+    createDivColor.className = 'color';
+    createDivColor.style.border = '1px solid black';
+        
+    const sectionColorPalette = document.querySelector('#color-palette');
+    sectionColorPalette.appendChild(createDivColor);
 }
 
-// Funções 
+//Define a primeira div de classe color como preta
+const divsColor = document.getElementsByClassName('color');
+divsColor[0].style.backgroundColor = 'black';
 
+//Laço for que insere cor aleatório nas divs de classe color a partir do 2º item
+for (let index = 1; index < divsColor.length; index += 1) {
+    divsColor[index].style.backgroundColor = createRandomRGB();
+}
+
+
+
+
+// Funções 
+function createRandomRGB () {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+}
